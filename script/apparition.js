cb7ce485d18d8bdb6e166e3dcb043eb1bc39a1a2
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2022-10-01 22:23:50 +0200

    Resolving issue with age and adding Doc for js files

--- script/apparition.js
+++ script/apparition.js
@@ -1,5 +1,8 @@
+/* Selecting all the elements with the class of hide. */
 const hiddenElements = document.querySelectorAll(".hide");
 
+/* A function that is called when the page loads and when the user scrolls. It is checking the position of the
+elements and adding a class to them when they are in the viewport. */
 scrollAnim = () => {
 	let windowHt = window.innerHeight;
 	hiddenElements.forEach(elements => {
@@ -10,6 +13,8 @@ scrollAnim = () => {
 	});
 }
 
+/* Listening for the DOM to load and then calling the scrollAnim function. */
 document.addEventListener("DOMContentLoaded", scrollAnim);
 
+/* Listening for the user to scroll and then calling the scrollAnim function. */
 window.addEventListener('scroll', scrollAnim);
--- script/script.js
+++ script/script.js
@@ -1,12 +1,20 @@
+/* Getting the element with the id of 'count' and storing it in the variable countEl. */
 const countEl = document.getElementById('count');
 
+/* Calling the function setAge() */
 setAge();
 
+/* Calling the function updateVisitCount() */
 updateVisitCount();
 
+/**
+ * Define the age of the user (me in this case)
+ */
 function setAge() {
     let d = new Date();
-    if (d.getMonth() > 4 && d.getDate() >= 20)
+    /* Checking if the current date is after May 20th. If it is, it will display the current year - 2003. If it is not, it
+    will display the current year - 1 - 2003. */
+    if (d.getMonth() > 4 || (d.getDate() >= 20 && d.getMonth() === 4))
         document.getElementById("age").innerHTML = (d.getFullYear() - 2003);
     else {
         document.getElementById("age").innerHTML = (d.getFullYear() - 1 - 2003);
@@ -14,6 +22,10 @@ function setAge() {
 }
 
 
+/**
+ * It changes the theme of the website by changing the href of the theme_css element
+ * @param color - The color of the theme you want to change to.
+ */
 function changeTheme(color) {
     if (color === 'dark')
         document.getElementById('theme_css').href = './css/color/themeDark.css';
@@ -25,20 +37,38 @@ function changeTheme(color) {
         document.getElementById('theme_css').href = './css/color/themePurple.css';
 }
 
+/**
+ * When the user hovers over the image with the id of 'id', change the image to the image at the path 'path'.
+ * @param id - The id of the image you want to change.
+ * @param path - The path to the image you want to display when the mouse is over the image.
+ */
 function onHover(id, path) {
     document.getElementById(id).src = path;
 }
 
+/**
+ * "When the mouse leaves the image, change the image to the one specified in the path variable."
+ *
+ * The function is called in the HTML code like this:
+ * @param id - The id of the image you want to change.
+ * @param path - The path to the image you want to display when the mouse is not hovering over the image.
+ */
 function offHover(id, path) {
     document.getElementById(id).src = path;
 }
 
+/**
+ * It will copy the text "YuanMeng#3647" to your clipboard.
+ */
 function copyContent() {
     navigator.clipboard.writeText("YuanMeng#3647");
     alert("Id copy to your clipboard")
 }
 
 
+/**
+ * It fetches the data from the API, then it updates the HTML element with the value of the data
+ */
 function updateVisitCount() {
     fetch('https://api.countapi.xyz/hit/lacroix.gq')
         .then(res => res.json())
